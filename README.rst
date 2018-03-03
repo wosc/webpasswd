bf73c4881c9bce30f5100dbdc405264253b9a0f1
Wolfgang Schnerring <wosc@wosc.de>
2018-03-03 17:46:36 +0100

    Create skeleton

---
+++ README.rst
@@ -0,0 +1,27 @@
+==============================
+Web-based UNIX password change
+==============================
+
+This packages provides a web UI for the UNIX ``passwd`` command, e.g. so that
+SFTP-only user accounts can still change their password.
+
+WARNING: The passwords are passed in plaintext via popen from the CGI process to
+the helper process that runs under sudo. So maybe don't use this in an
+adversarial environment where people might spy on your process list or something.
+
+
+Usage
+=====
+
+Set up your webserver to run the CGI script. Here's an example apache
+configuration snippet::
+
+    ScriptAlias /passwd /path/to/venv/bin/webpasswd-cgi
+
+As changing the password for another user requires root access, you also need to
+setup passwordless sudo execution for the ``webpasswd-change`` helper.
+Add a rule like this, e.g. in ``/etc/sudoers.d/webpasswd``::
+
+    www-data ALL=NOPASSWD:/path/to/venv/bin/webpasswd-change
+
+``www-data`` is the user the webserver that executes the CGI script runs as.
---
+++ setup.py
@@ -0,0 +1,53 @@
+"""Change a UNIX password via web UI
+"""
+from setuptools import setup, find_packages
+import glob
+
+
+setup(
+    name='ws.webpasswd',
+    version='2.0.0.dev0',
+
+    install_requires=[
+        'flask',
+        'python-pam',
+        'setuptools',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'webpasswd-change = ws.webpasswd.update:main',
+            'webpasswd-cgi = ws.webpasswd.web:main',
+        ],
+    },
+
+    author='Wolfgang Schnerring <wosc@wosc.de>',
+    author_email='wosc@wosc.de',
+    license='ZPL 2.1',
+    url='https://github.com/wosc/webpasswd',
+
+    description=__doc__.strip(),
+    long_description='\n\n'.join(open(name).read() for name in (
+        'README.rst',
+        'CHANGES.txt',
+    )),
+
+    classifiers="""\
+License :: OSI Approved :: Zope Public License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: Implementation :: CPython
+"""[:-1].split('\n'),
+
+    namespace_packages=['ws'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob('*.txt'))],
+    zip_safe=False,
+)
